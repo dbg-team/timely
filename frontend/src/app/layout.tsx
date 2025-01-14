a324c4d8ea115f7382c574c36b06d12ef3147bc1
edge <j2hyeon02@kaist.ac.kr>
2025-01-15 00:50:31 +0900

    add tailwind css and shadcn ui

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+// import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,7 +26,16 @@ export default function RootLayout({
     <html>
       <body>
       <h1><a href="/">WEB</a></h1>
+      <ol>
+        <li><a href="/read/1">html</a></li>
+        <li><a href="/read/2">css</a></li>
+      </ol>
         {children}
+      <ul>
+        <li><a href="/create">Create</a></li>
+        <li><a href="/update/1">Update</a></li>
+        <li><input type="button" value="delete"></input></li>
+      </ul>
       </body>
     </html>
   );
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
     <h2>Hello!</h2>
     Hello, Next JS!
+    <Link href="/users">Users</Link>
     </>
   );
 }
